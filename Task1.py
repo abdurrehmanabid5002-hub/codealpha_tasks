6ef5d9b87d78d95d27648197e9666cc742e434e3
AbdurRehmanAbid <abdurrehmanabid5002@gmail.com>
2026-07-15 20:39:44 +0500

    code

--- Task1.py
+++ Task1.py
@@ -37,3 +37,8 @@ for i in range(rows):
         else:
             print(" ", end="")
     print()
+# lopp
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
