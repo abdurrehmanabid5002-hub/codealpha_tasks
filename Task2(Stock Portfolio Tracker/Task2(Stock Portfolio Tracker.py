0de58f94ee40bb0bc145c0d6ce9a0d1727aebb78
AbdurRehmanAbid <abdurrehmanabid5002@gmail.com>
2026-07-22 23:32:59 +0500

    fixed issue

--- Task2(Stock Portfolio Tracker/Task2(Stock Portfolio Tracker.py
+++ Task2(Stock Portfolio Tracker/Task2(Stock Portfolio Tracker.py
@@ -1,24 +1,30 @@
-stock_price = {"AAPL": 180, "TSLA": 250, "MSFT": 350, "GOOGL": 150}
-Total_investment = 0
-while True:
-    stocks = input(("enter the stock to buy :")).upper()
-    if stocks.lower() == "exit":
-        break
-    else:
-        quantity = int(input("enter the the quantity to buy stocks: "))
-        if stocks in stock_price:
-            price = stock_price[stocks]
-
-            Investment = price * quantity
-            print(f"{stocks} {quantity} Sharres = {Investment}$")
-            Investment1 = f"{stocks} {quantity} Sharres = {Investment}$"
-            Total_investment += Investment
-            print(Total_investment)
-
+class portfolio_tracker:
+    stock_price = {"AAPL": 180, "TSLA": 250, "MSFT": 350, "GOOGL": 150}
+    Total_investment = 0
+    portfolio=[]
+    while True:
+        stocks = input(("enter the stock to buy :")).upper()
+        if stocks.lower() == "exit":
+            break
         else:
-            print("Enter write stock ")
+            quantity = int(input("enter the the quantity to buy stocks: "))
+            if stocks in stock_price:
+                price = stock_price[stocks]
 
-with open("Portfolio.txt", "w") as f:
-    f.write(str(Investment1))
-    f.write(f"\nTotal Investment = {Total_investment}$")
-print("File is written succesfully ")
+                Investment = price * quantity
+                print(f"{stocks} {quantity} Sharres = {Investment}$")
+                Investment_ = f"{stocks} {quantity} Shares = {Investment}$"
+                Total_investment += Investment
+                print(f"Total_investment of all these shares is {Total_investment}")
+                portfolio.append(Investment_)
+
+            else:
+                print("Enter the correct stock ")
+
+    with open("Portfolio.txt", "w") as f:
+            for items in portfolio:
+                f.write(f"{items}\n")
+            f.write(f"\nTotal Investment = {Total_investment}$")
+    print("File is written succesfully ")
+
+portfolio_tracker()
